d8e6f3412c4bcf56c3b63dde2145d5e073bda366
Nazar Valchyshyn <nvalja@gmail.com>
2021-02-02 05:57:24 +0200

    Updates

---
+++ precache-manifest.21ec755990cbc659c9e45e23da58a96b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5553a4ee0975271f1282beed1fb2901",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e67d86cc94af90d9f13",
+    "url": "./static/css/main.06d11d8c.chunk.css"
+  },
+  {
+    "revision": "a384fa81a91cb717f2fb",
+    "url": "./static/js/2.cffae293.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.cffae293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e67d86cc94af90d9f13",
+    "url": "./static/js/main.0a65d98d.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
